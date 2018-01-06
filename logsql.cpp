b61606d01eb688b021c6707c732cd03a9789a4c0
Ashley Lavery <ashley@sa-irc.com>
2018-01-06 20:13:43 +0000

    Connect to the MySQL server

--- logsql.cpp
+++ logsql.cpp
@@ -1,11 +1,60 @@
 #include <znc/Modules.h>
 
+#include <mysql/mysql.h>
+
 class CLogSQL : public CModule {
 public:
   MODCONSTRUCTOR(CLogSQL) {}
 
-  ~CLogSQL() override {}
+  ~CLogSQL() override {
+    if(mysqlConn)
+      mysql_close(mysqlConn);
+  }
+
+  bool OnLoad(const CString& sArgs, CString& sMessage) override;
+
+  EModRet OnChanMsg(CNick& Nick, CChan& Channel, CString& sMessage) override;
+
+private:
+  CString sHost;
+  CString sUserDB;
+  CString sPassDB;
+  CString sDatabase;
+
+  MYSQL *mysqlConn;
 };
+
+bool CLogSQL::OnLoad(const CString& sArgs, CString& sMessage) {
+  sHost = sArgs.Token(0);
+  sUserDB = sArgs.Token(1);
+  sPassDB = sArgs.Token(2);
+  sDatabase = sArgs.Token(3);
+
+  mysqlConn = mysql_init(NULL);
+
+  if(!mysqlConn) {
+    sMessage = "Could not initiate a MySQL connection";
+    
+    DEBUG("mysql_init() failed.");
+
+    return false;
+  }
+
+  if(!mysql_real_connect(mysqlConn, sHost.c_str(), sUserDB.c_str(), sPassDB.c_str(), sDatabase.c_str(), 0, NULL, 0)) {
+    sMessage = "Could not connect to the MySQL server";
+
+    CString err = mysql_error(mysqlConn);
+    DEBUG("Could not connect to MySQL server. " + err);
+
+    return false;
+  }
+
+  return true;
+}
+
+CModule::EModRet CLogSQL::OnChanMsg(CNick& Nick, CChan& Channel, CString& sMessage) {
+  return CONTINUE;
+}
 
 template<> void TModInfo<CLogSQL>(CModInfo& Info) {
   Info.SetHasArgs(true);
